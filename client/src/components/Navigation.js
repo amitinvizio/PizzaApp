408a57e5aacae93a9d849b00c0f900878bde16a5
amitinvizio <Amit@inviziosolutions.com>
2021-07-14 14:12:44 +0530

    Updated Product Component

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from '../pages/Home'
+import Products from '../pages/Products'
+import Cart from '../pages/Cart'
+
+const logoStyle = {
+  height: 80,
+}
+
+const cartStyle = {
+  background: '#F59E0D',
+  display: 'flex',
+  padding: '6px 12px',
+  borderRadius: '50px'
+}
+
+const Navigation = () => {
+  return (
+    <>
+      <Router>
+        <nav className="container mx-auto flex items-center justify-between py-5">
+          <Link to="/">
+            <img style={logoStyle} src="/images/logo.jpg" alt="logo" />
+          </Link>
+          <ul className="flex items-center">
+            <li className="ml-6">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="ml-6"><Link to="/products">Products</Link></li>
+            <li className="ml-6">
+              <Link to="/cart">
+                <div style={cartStyle}>
+                  <span className="ml-2">10</span>
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/" component={Home} exact>
+          </Route>
+          <Route path="/products" component={Products}>
+          </Route>
+          <Route path="/cart" component={Cart}>
+          </Route>
+        </Switch>
+        
+      </Router>
+    </>
+  )
+}
+
+export default Navigation
---
+++ client/src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+      <h1>This page is from About Routing</h1>
+    </>
+  )
+}
+
+export default About
